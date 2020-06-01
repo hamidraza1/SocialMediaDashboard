dc822ef486499fbed6df9ad038e4009ab79b447c
hamidraza1 <hrazaa306@gmail.com>
2020-06-01 14:11:39 +0200

    Updates

---
+++ precache-manifest.c1a571150c62245a3b123e09d67bf260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b083ccd316bd104bdf5ab7a3b86cd036",
+    "url": "/SocialMediaDashboard/index.html"
+  },
+  {
+    "revision": "9e5ad38d8684ad824797",
+    "url": "/SocialMediaDashboard/static/css/main.c9f810f0.chunk.css"
+  },
+  {
+    "revision": "79161fa3e8c57242f415",
+    "url": "/SocialMediaDashboard/static/js/2.b8365c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SocialMediaDashboard/static/js/2.b8365c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5ad38d8684ad824797",
+    "url": "/SocialMediaDashboard/static/js/main.fd4f5576.chunk.js"
+  },
+  {
+    "revision": "e10372d2c67dd6339708",
+    "url": "/SocialMediaDashboard/static/js/runtime-main.e63aae64.js"
+  }
+]);
